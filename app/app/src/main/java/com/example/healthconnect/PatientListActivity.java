4a535732fc7b01c91427127ccba3a5bff2c33753
tiennguyenvan <nguyentien.jobs@gmail.com>
2024-11-02 21:45:03 -0700

    Finished All Screens

--- app/app/src/main/java/com/example/healthconnect/PatientListActivity.java
+++ app/app/src/main/java/com/example/healthconnect/PatientListActivity.java
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class PatientListActivity extends AppCompatActivity {
 

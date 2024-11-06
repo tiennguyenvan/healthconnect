3d9da0a4d16b8bfbfedc8f93f4cf93b39b9e9fb0
PhyoThor <diazphyo@gmail.com>
2024-11-05 17:44:47 -0800

    New Appoinment and Appoinment Update form built and code for Update appointment completed.

--- app/app/src/main/java/com/example/healthconnect/appointmentNew.java
+++ app/app/src/main/java/com/example/healthconnect/appointmentNew.java
@@ -8,28 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    private DatabaseHelper dbHelper;
+public class appointmentNew extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_appointment_new);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        dbHelper = DatabaseHelper.getInstance(this);
-
-        $.in(this).onClick(R.id.btPatients).goToScreen(PatientListActivity.class);
-        $.in(this).onClick(R.id.btAppointments).goToScreen(appointmentUpdate.class);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        dbHelper.closeDatabase();
     }
 }

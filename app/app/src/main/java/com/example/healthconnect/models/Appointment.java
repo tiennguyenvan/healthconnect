c9e0ddaa75fbf06e77173bbeea567f8231f4650c
Bruno Beserra <bruno.n.beserra@gmail.com>
2024-11-10 21:11:24 -0800

    Update Appointment.java

--- app/app/src/main/java/com/example/healthconnect/models/Appointment.java
+++ app/app/src/main/java/com/example/healthconnect/models/Appointment.java
@@ -8,23 +8,32 @@ import java.util.List;
 public class Appointment implements Serializable {
     private long appointment_id;
     private long patient_id;
-    private String patientName;
-    private String dateOfAppointment;
-    private int durationAppointment;
+    private String startDate;
+    private String endDate;
+    private String diagnosis;
+    private String[] symptoms;
+    private String prescriptionDetail;
 
     public Appointment(){}
 
-    public Appointment(long patient_id, String patientName, String dateOfAppointment, int durationAppointment)
+    public Appointment(long patient_id, String startDate, String endDate)
     {
         this.patient_id = patient_id;
-        this.patientName = patientName;
-        this.dateOfAppointment = dateOfAppointment;
-        this.durationAppointment = durationAppointment;
+        this.startDate = startDate;
+        this.endDate = endDate;
     }
 
-    public Appointment(long appointment_id, long patient_id, String patientName, String dateOfAppointment, int durationAppointment) {
-        this(patient_id, patientName, dateOfAppointment, durationAppointment);
+    public Appointment(long appointment_id, long patient_id, String startDate, String endDate) {
+        this(patient_id, startDate, endDate);
         this.appointment_id = appointment_id;
+    }
+
+    public Appointment(long appointment_id, long patient_id, String startDate, String endDate, String diagnosis, String[] symptoms, String prescriptionDetail)
+    {
+        this(appointment_id, patient_id, startDate, endDate);
+        this.diagnosis = diagnosis;
+        this.symptoms = symptoms;
+        this.prescriptionDetail = prescriptionDetail;
     }
 
     public long getAppointment_id() {
@@ -43,36 +52,52 @@ public class Appointment implements Serializable {
         this.patient_id = patient_id;
     }
 
-    public String getPatientName() {
-        return patientName;
+    public String getStartDate() {
+        return startDate;
     }
 
-    public void setPatientName(String patientName) {
-        this.patientName = patientName;
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
     }
 
-    public String getDateOfAppointment() {
-        return dateOfAppointment;
+    public String getEndDate() {
+        return endDate;
     }
 
-    public void setDateOfAppointment(String dateOfAppointment) {
-        this.dateOfAppointment = dateOfAppointment;
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
     }
 
-    public int getDurationAppointment() {
-        return durationAppointment;
+    public String getDiagnosis() {
+        return diagnosis;
     }
 
-    public void setDurationAppointment(int durationAppointment) {
-        this.durationAppointment = durationAppointment;
+    public void setDiagnosis(String diagnosis) {
+        this.diagnosis = diagnosis;
     }
 
-    public static List<Appointment> demoData() {
-        List<Appointment> demoData = new ArrayList<>();
-        demoData.add(new Appointment(1, "Tim Nguyen", "2024-09-11", 30));
-        demoData.add(new Appointment(2,"Phyo Thaw", "2024-09-10", 60));
-        demoData.add(new Appointment(3, "Bruno Beserra", "2024-10-11", 22));
-        return demoData;
+    public String[] getSymptoms() {
+        return symptoms;
     }
+
+    public void setSymptoms(String[] symptoms) {
+        this.symptoms = symptoms;
+    }
+
+    public String getPrescriptionDetail() {
+        return prescriptionDetail;
+    }
+
+    public void setPrescriptionDetail(String prescriptionDetail) {
+        this.prescriptionDetail = prescriptionDetail;
+    }
+
+    //    public static List<Appointment> demoData() {
+//        List<Appointment> demoData = new ArrayList<>();
+//        demoData.add(new Appointment(1, "Tim Nguyen", "2024-09-11", 30));
+//        demoData.add(new Appointment(2,"Phyo Thaw", "2024-09-10", 60));
+//        demoData.add(new Appointment(3, "Bruno Beserra", "2024-10-11", 22));
+//        return demoData;
+//    }
 }
 
